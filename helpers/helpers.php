2eb0304af1cb25e29d317f79cda4b24dfd67377e
nguyenanhung <info@nguyenanhung.com>
2023-11-05 03:02:23 +0700

    first release

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,1 +1,17 @@
 <?php
+if (!function_exists('cli_input_ask')) {
+    function cli_input_ask($question, $answers = null)
+    {
+        echo $question . ' ';
+        $handle = fopen("php://stdin", "r");
+        $answer = fgets($handle);
+        fclose($handle);
+        $answer = trim($answer);
+        if ($answers != null) {
+            if (!in_array($answer, $answers)) {
+                return cli_input_ask($question, $answers);
+            }
+        }
+        return $answer;
+    }
+}
--- test/test.php
+++ test/test.php
@@ -1,2 +1,14 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+
+
+$question = 'are you an idiot? (y/n)';
+$answers = ['y', 'n'];
+
+if (cli_input_ask($question, $answers) === 'y') {
+    die('ngu vl');
+} elseif (cli_input_ask($question, $answers) === 'n') {
+    die('Bạn thì kinh rồi!');
+}
+
+
